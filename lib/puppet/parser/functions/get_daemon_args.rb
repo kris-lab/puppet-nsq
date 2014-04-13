b41c617d8f31b739f9467f4074dbe8476df6d1b4
Chris Stasiak <me@krzysztof-stasiak.com>
2014-04-13 21:15:40 +0200

    Added shellescape

--- lib/puppet/parser/functions/get_daemon_args.rb
+++ lib/puppet/parser/functions/get_daemon_args.rb
@@ -1,7 +1,8 @@
+require 'shellwords'
 module Puppet::Parser::Functions
   newfunction(:get_daemon_args, :type => :rvalue) do |args|
     options = args[0]
     config = options.split("\n")
-    config.map{|v| "-#{v}" }.join(' ')
+    config.map{|v| "-#{v}" }.join(' ').shellescape
   end
 end
