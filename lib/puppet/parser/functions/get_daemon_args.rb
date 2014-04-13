932574fc1142ba06211050695c7eecf594dbb6ac
Chris Stasiak <me@krzysztof-stasiak.com>
2014-04-13 21:17:47 +0200

    Fixed init script

--- lib/puppet/parser/functions/get_daemon_args.rb
+++ lib/puppet/parser/functions/get_daemon_args.rb
@@ -1,8 +1,7 @@
-require 'shellwords'
 module Puppet::Parser::Functions
   newfunction(:get_daemon_args, :type => :rvalue) do |args|
     options = args[0]
     config = options.split("\n")
-    config.map{|v| "-#{v}" }.join(' ').shellescape
+    config.map{|v| "-#{v}" }.join(' ')
   end
 end
